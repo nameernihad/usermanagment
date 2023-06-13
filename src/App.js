d764228683a5f0b8ce55f9b5fedeafd7892c4c14
Nameer Nihad <nameernihadpc@gmail.com>
2023-06-13 21:59:41 +0530

    user login and register completed usin jwt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Secret from "./pages/secret";
+import  "react-toastify/dist/ReactToastify.css";
+
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/register" element={<register/>} />
-        <Route exact path="/login" element={<login/>} />
-        <Route exact path="/" element={<secret/>} />
+        <Route exact path="/register" element={<Register/>} />
+        <Route exact path="/login" element={<Secret/>} />
+        <Route exact path="/" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
